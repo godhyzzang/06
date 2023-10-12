8fe233cb33fb4aa21f70d7e74f42ea8ddfccc9b8
godhyzzang <amksh0914@gmail.com>
2023-10-12 16:05:16 +0900

    6회차 - 실습 3 : 여러가지 함수 구현
    6회차 - 실습 3 : 여러가지 함수 구현

--- main.c
+++ main.c
@@ -1,11 +1,29 @@
 #include <stdio.h>
-void func1(void) {
-	int x;
-	printf("func1 x is at % p\n", &x);
+int sumTwo(int a, int b) {
+	
+	return a + b;
+	
+}
+
+int square(int n) {
+	return n * n;
+}
+
+int get_max(int x, int y) {
+	if (x < y) 
+		return y;
+	else {
+		return x;
+	}
 }
 int main(void) {
-	int x;
-	printf("main x is at % p\n", &x);
-	func1();
+	
+	
+	printf("sumTwo result : %i\n", sumTwo(2, 3));
+	printf("squre result : %i\n", square(3));
+	printf("get_max : %i\n", get_max(5, 10));
+
+
+
 	return 0;
 }
